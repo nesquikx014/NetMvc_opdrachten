c8c5e519fc91df60ade4c624b337140bab0b3dab
Nazli Eroglu <naz@example.com>
2025-05-13 13:05:47 +0200

    tweede commit

---
+++ solutions/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("dit int: 88");
+Console.WriteLine("dit string: mariooooooo");
+Console.WriteLine("dit bool: False");
+Console.WriteLine("dit float: 14");
