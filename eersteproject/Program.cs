5bdfa098886e5db5bb74816a6abfb52d573cae44
Nazli Eroglu <naz@example.com>
2025-05-13 12:43:21 +0200

    eerste commit

---
+++ eersteproject/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!"); 
+Console.WriteLine("dit int: 14");
+Console.WriteLine("dit string: Toad");
+Console.WriteLine("dit bool: False");
+Console.WriteLine("dit float: 8");
